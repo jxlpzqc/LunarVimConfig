42fb617ad741a4bddbeab58ec10104d221a0b232
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-07-17 19:53:34 +0800

    chore: change c indent to 4 spaces.

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,3 +1,4 @@
-vim.opt_local.ts = 8
-vim.opt_local.sw = 8
-vim.opt_local.expandtab = false
+local tabstop = 4
+vim.opt_local.ts = tabstop
+vim.opt_local.sw = tabstop
+vim.opt_local.expandtab = true
